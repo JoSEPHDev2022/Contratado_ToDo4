bcc4c32bb7830011f30a72d94ce465671eeefdfe
LuizFdev <jl_ferreira16@hotmail.com>
2022-07-11 18:29:46 -0300

    Refazendo o código
    Uma nova iteração do código. Depois de muitos testes, cheguei a conclusção que um novo código era necessário. Agora falta apenas uma última validação de dados para conclusão do projeto.

--- contratado.py
+++ contratado.py
@@ -1,33 +1,47 @@
-vagas = {
-    'analista de dados': ['python', 'powerbi', 'sql', 'boa comunicação'],
-    'cientista de dados': ['python', 'banco de dados', 'machine learning', 'resolução de problemas', 'estatística']
-}
+keywords_analista = {'analista de dados': ['python', 'powerbi', 'sql', 'boa comunicação']}
+keywords_cientista = {'cientista de dados': ['python', 'banco de dados', 'machine learning', 'resolução de problemas', 'estatística']}
 
+registro_de_aplicacoes = []
 aplicantes = {}
-resumos = {}
-lista_participantes = []
-lista_resumos = []
 
-num_candidatos = int(input("Quantos candidatos irão ser aplicados?: "))
+while True:
+    aplicantes.clear()
+    aplicantes['nome'] = input("Nome: ")
 
-for ciclos in range(num_candidatos):
-    aplicantes['nome'] = input("Nome do candidato: ")
-    aplicantes['vaga'] = input("Para qual vaga está se inscrevendo: ")
-    resumos['resumo'] = input("Insira o resumo: ")
-    lista_participantes.append(aplicantes.copy())
-    lista_resumos.append(resumos.copy())
+    while True: 
+        aplicantes['vaga'] = input("Qual a vaga que quer se aplicar: ").lower()
+        if aplicantes['vaga'] in ('analista de dados cientista de dados'):
+            break
+        print("A vaga inserida não está disponível!")
+    aplicantes['resumo'] = input("Insira o resumo do currículo do aplicante:\n")
+    registro_de_aplicacoes.append(aplicantes.copy())
 
+    while True:
+        adicionar = input("Deseja adicionar mais algum participante?[S/N]: ").lower()
+        if adicionar in 'sn':
+            break
+        print("Erro. Responda com 'S' ou 'N'.")
+    if adicionar == 'n':
+        break
+        
+print("=*"*30)
+print(f"Tivemos {len(registro_de_aplicacoes)} aplicações, sendo elas:\n")
 
-print('\n'+"=*"*35)
-print(f"No total, foram registradas as aplicações de {num_candidatos} participantes:\n")
+cont = 0 
+for candidaturas in registro_de_aplicacoes:
+    cont += 1
+    print(f"Aplicação {cont}: {candidaturas}")
 
-aplicacao_cont = 0
-for dicionarios in lista_participantes:
-    aplicacao_cont += 1
-    print(f"Aplicação {aplicacao_cont}: {dicionarios}")
+print("=*"*30)
+print("Dessas aplicações:\n")
+candidaturas_analista = 0
+candidaturas_cientista = 0
 
-resum_cont = 0
-for resumos in lista_resumos:
-    for valores in resumos.values():
-        resum_cont += 1
-        print(f"Resumo {resum_cont}: {valores}")
+for candidatos in registro_de_aplicacoes:
+    if candidatos['vaga'] == 'analista de dados':
+        candidaturas_analista += 1
+    if candidatos['vaga'] == 'cientista de dados':
+        candidaturas_cientista += 1
+
+print(f"{candidaturas_analista} foram para Analista de Dados.")
+print(f"{candidaturas_cientista} foram para Cientista de Dados.")
