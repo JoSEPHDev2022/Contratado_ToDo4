186734543ef10d03a0346d04eef3e4e8b4c54719
LuizFdev <jl_ferreira16@hotmail.com>
2022-07-13 15:19:16 -0300

    Versão final do projeto.
    Adicionando os códigos em funções, inserindo check de keywords dos currículos dos participantes e polindo a saída de dados.

--- contratado.py
+++ contratado.py
@@ -1,47 +1,102 @@
+import time as t
+
 keywords_analista = {'analista de dados': ['python', 'powerbi', 'sql', 'boa comunicação']}
 keywords_cientista = {'cientista de dados': ['python', 'banco de dados', 'machine learning', 'resolução de problemas', 'estatística']}
 
 registro_de_aplicacoes = []
 aplicantes = {}
 
-while True:
-    aplicantes.clear()
-    aplicantes['nome'] = input("Nome: ")
+lista_aprovados_an = []
+lista_aprovados_ci = []
 
-    while True: 
-        aplicantes['vaga'] = input("Qual a vaga que quer se aplicar: ").lower()
-        if aplicantes['vaga'] in ('analista de dados cientista de dados'):
-            break
-        print("A vaga inserida não está disponível!")
-    aplicantes['resumo'] = input("Insira o resumo do currículo do aplicante:\n")
-    registro_de_aplicacoes.append(aplicantes.copy())
-
+def inserindo_candidatos():
+    print("=*"*50)
+    print("Bem vindo ao Sistema de Registro de Aplicações de Candidatos - SRAC")
+    print("A seguir, insira os nomes, as pretensões de vagas e o resumo dos currículos dos participantes.")
+    print("=*"*50)
+    t.sleep(1)
     while True:
-        adicionar = input("Deseja adicionar mais algum participante?[S/N]: ").lower()
-        if adicionar in 'sn':
-            break
-        print("Erro. Responda com 'S' ou 'N'.")
-    if adicionar == 'n':
-        break
+        aplicantes.clear()
+        aplicantes['nome'] = input("Nome do candidato: ")
+
+        while True: 
+            aplicantes['vaga'] = input("Para qual vaga é a aplicação: ").lower()
+            if aplicantes['vaga'] in ('analista de dados cientista de dados'):
+                break
+            print("A vaga inserida não está disponível! Seleciona entre 'Analista de Dados' e 'Cientista de Dados'.")
+        aplicantes['resumo'] = input("Insira o resumo do currículo do aplicante:\n").lower()
+        print(f"\nAdicionando registro do(a) {aplicantes['nome']}...")
+        t.sleep(1)
+        print("Registro adicionado!")
+        registro_de_aplicacoes.append(aplicantes.copy())
+
+        while True:
+            adicionar = input("\nDeseja adicionar mais algum participante?[S/N]: ").lower()
+            if adicionar in 'sn':
+                break
+            print("Erro. Responda com 'S' ou 'N'.")
+        if adicionar == 'n':
+            relatorio_aplicacoes()
         
-print("=*"*30)
-print(f"Tivemos {len(registro_de_aplicacoes)} aplicações, sendo elas:\n")
+def relatorio_aplicacoes():
+    print("=*"*50)
+    t.sleep(1)
+    print(f"Tivemos {len(registro_de_aplicacoes)} aplicações para nossas vagas, sendo elas:\n")
+    cont = 0 
+    for candidaturas in registro_de_aplicacoes:
+        cont += 1
+        print(f"Aplicação {cont}: {candidaturas['nome']} se aplicou para {candidaturas['vaga']}")
+    print()
+    print("=*"*50)
+    t.sleep(1)
+    print("Dessas aplicações:\n")
+    candidaturas_analista = 0
+    candidaturas_cientista = 0
 
-cont = 0 
-for candidaturas in registro_de_aplicacoes:
-    cont += 1
-    print(f"Aplicação {cont}: {candidaturas}")
+    for candidatos in registro_de_aplicacoes:
+        if candidatos['vaga'] == 'analista de dados':
+            candidaturas_analista += 1
+        if candidatos['vaga'] == 'cientista de dados':
+            candidaturas_cientista += 1
 
-print("=*"*30)
-print("Dessas aplicações:\n")
-candidaturas_analista = 0
-candidaturas_cientista = 0
+    print(f"{candidaturas_analista} foram para Analista de Dados.")
+    print(f"{candidaturas_cientista} foram para Cientista de Dados.")
+    print("=*"*50)
+    check_resumos_aprovados()
 
-for candidatos in registro_de_aplicacoes:
-    if candidatos['vaga'] == 'analista de dados':
-        candidaturas_analista += 1
-    if candidatos['vaga'] == 'cientista de dados':
-        candidaturas_cientista += 1
+def check_resumos_aprovados():
+    for candidatos in registro_de_aplicacoes:
+        if candidatos['vaga'] == 'analista de dados':
+            for keywords_1 in keywords_analista.values():
+                for palavras_1 in keywords_1:
+                    if palavras_1 in candidatos['resumo']:
+                        lista_aprovados_an.append(candidatos)
+                        break
+                    continue
+                break
+        if candidatos['vaga'] == 'cientista de dados':
+            for keywords_2 in keywords_cientista.values():
+                for palavras_2 in keywords_2:
+                    if palavras_2 in candidatos['resumo']:
+                        lista_aprovados_ci.append(candidatos)
+                        break
+                    continue
+                break
+    t.sleep(1)
+    print("Nome e Resumo dos candidatos aprovados para Analista de Dados:\n")
+    for aprovados in lista_aprovados_an:
+        print(f"Nome: {aprovados['nome']}\nResumo >> {aprovados['resumo']}\n")
+    print("=*"*50)
 
-print(f"{candidaturas_analista} foram para Analista de Dados.")
-print(f"{candidaturas_cientista} foram para Cientista de Dados.")
+    t.sleep(1)
+    print("Nome e Resumo dos candidatos aprovados para Cientista de Dados:\n")
+    for aprovados_2 in lista_aprovados_ci:
+        print((f"Nome: {aprovados_2['nome']}\nResumo >> {aprovados_2['resumo']}\n"))
+    print("=*"*50)
+    t.sleep(1)
+    print("Finalizando...")
+    t.sleep(1)
+    print("Programa Finalizado!")
+    exit()
+
+inserindo_candidatos()
